5e326222ebf640c908cc843d7ee08ee0da2fef14
kangmin <rlarkdals579@naver.com>
2022-04-30 16:50:19 -0400

    dfs

--- Step3/DFS,BFS1/음료수 얼려먹기.py
+++ Step3/DFS,BFS1/음료수 얼려먹기.py
@@ -25,7 +25,7 @@ for i in range(n):
 result = 0
 for i in range(n):
     for j in range(m):
-        if dfs(i, j) == True:
-            result += 1
+        if dfs(i,j) == True:
+            result +=1
 
 print(result)
